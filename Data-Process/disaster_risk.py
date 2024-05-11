3d4a9dce1d4281a15cbd25a486019a263913a59a
leonw774 <leonw774@gmail.com>
2024-05-11 16:41:02 +0800

    added population density

--- Data-Process/disaster_risk.py
+++ Data-Process/disaster_risk.py
@@ -1,6 +1,6 @@
 import json
 
-import tqdm
+from tqdm import tqdm
 import shapely
 import shapely.ops
 
@@ -49,13 +49,12 @@ danger_slopes = [
     for feature in danger_slopes_geojson['features']
     if feature['properties']['level'] != '第五類'
 ]
-# print(danger_slopes)
 
 danger_slope_vuln_point = {
     vname: 0
     for vname in vnames
 }
-for vname in tqdm(vnames):
+for vname in tqdm(vnames, ncols=100):
     vborder = village_borders_shapes[vname]
     for slope in danger_slopes:
         # check intersect
@@ -89,23 +88,18 @@ old_house_points = [
     if feature["properties"]["age_2021"] < 20 and feature["geometry"]is not None
 ]
 
-danger_slope_vuln_point = {
+old_house_vuln_point = {
     vname: 0
     for vname in vnames
 }
-
 for old_house in old_house_points:
     for vname in vnames:
         vborder = village_borders_shapes[vname]
         # check intersect
         if not shapely.intersects(old_house, vborder):
             continue
-        
-        danger_slope_vuln_point[vname] += 1
+        old_house_vuln_point[vname] += 1
 		
-# print(danger_slope_vuln_point)
-		
-
 ###### Liquefaction
 
 liquefaction_geojson_path = 'data/work_soil_liquefaction.geojson'
@@ -120,7 +114,7 @@ liquefaction_vuln_point = {
     vname: 0
     for vname in vnames
 }
-for vname in tqdm(vnames):
+for vname in tqdm(vnames, ncols=100):
     vborder = village_borders_shapes[vname]
     vborder_area = shapely.area(vborder)
     for polygon, cls in liquefaction_polygon_class.items():
@@ -153,6 +147,48 @@ liquefaction_vuln_point = {
 for vname in vnames:
     village_vuln_point[vname] = (
         danger_slope_vuln_point[vname]
-        # + old_house_vuln_point[vname]
+        + old_house_vuln_point[vname]
         + liquefaction_vuln_point[vname]
     )
+
+# Population desity
+
+village_population_csv_path = 'data/111_village_populations.csv'
+with open(village_population_csv_path) as village_population_csv_file:
+    # we only want total population for each vil.
+    village_population_lines = village_population_csv_file.readlines()[4::3]
+    # we only want the 3rd and 5th column: village name and total population
+    village_population_lines_splited = [
+        line.split(b',', maxsplit=6)
+        for line in village_population_lines
+    ]
+village_population = dict([
+    (line_splits[3], int(line_splits[5]))
+    for line_splits in village_population_lines_splited
+])
+village_population['糖蔀里'] = village_population['糖?里']
+# print(village_population)
+
+# directly read from front end geojson
+taipei_village_geojson_path = (
+    '../Taipei-City-Dashboard-FE/public/mapData/'
+    'taipei_village.geojson'
+)
+with open(taipei_village_geojson_path, 'r') as taipei_village_geojson_file:
+    taipei_village_geojson = json.load(taipei_village_geojson_file)
+village_area = {
+    feature['properties']['VNAME']: (
+        feature['properties']['AREA'] / 1_000_000 # from m^2 to km^2
+    )
+    for feature in taipei_village_geojson['features']
+}
+
+# calculate the polulation density
+village_population_density = {
+    vname: round(village_population[vname] / village_area[vname], 2)
+    for vname in village_area
+}
+
+# Set Manager DB
+
+manager_engine = get_manager_engine()
